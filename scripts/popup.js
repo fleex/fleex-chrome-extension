f4b3a65c5d663178c87bb5e0b0ebffc12fac4f15
Guillaume Dupuy <g.duwip@gmail.com>
2013-11-01 14:51:15 +0100

    New: Styled login popup
    Also made it so that hitting 'Enter' submits the email and password

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,6 +2,7 @@ $(function(){
 	var serverBaseUrl = chrome.runtime.getManifest().permissions[1];
 
 	function onSuccess (data) {
+		$('body').removeClass('loading');
 		if(data!='True'){ 
 			onFailure(); 
 		} else{
@@ -10,12 +11,13 @@ $(function(){
 	}
 
 	function onFailure () {
-		// TODO
-		alert('TODO: Show login error message in extension\'s popup');
+		$('header').addClass('error');
 	}
 
 	// Login user
 	$('#submitButton').click(function(){
+		$('header').removeClass('error');
+		$('body').addClass('loading');
 		$.ajax({
 			type:'POST',
 			url:serverBaseUrl+'Account/BrowserExtensionLogin',
@@ -27,4 +29,12 @@ $(function(){
 			success:onSuccess
 		})
 	})
+
+	// Listen to 'Enter' keypresses
+	$('html').keyup(function (event) {
+        //Enter key code = 13
+        if (event.keyCode == '13') {
+            $('#submitButton').click();
+        }
+    });
 })
