d8830050af341a9b53b8a704058fcf97453a4b98
Guillaume Dupuy <g.duwip@gmail.com>
2013-11-07 17:32:24 +0100

    Fix: Pass data from extension to page + detect page language

--- scripts/fleex-tab.js
+++ scripts/fleex-tab.js
@@ -4,47 +4,57 @@ function fleexTab (tab) {
 	$.getJSON(chrome.extension.getURL('config.json'), function(settings) {
 		var serverBaseUrl = settings.serverBaseUrl;
 
-		// Inject local js dependencies
-		chrome.tabs.executeScript(tab.id, { file: 'scripts/jquery-1.8.3.js' });
+		// Detect page language
+		chrome.tabs.detectLanguage(tab.id, function(language){
+			iso6391Language = language;
+			
+			// Inject local js dependencies
+			chrome.tabs.executeScript(tab.id, { file: 'scripts/jquery-1.8.3.js' });
 
-		// Fetch (remote) or (static + dynamic) js dependencies
-		$.when.apply($, [
-			$.ajax({ url:serverBaseUrl+'/Scripts/libs/underscore-1.5.1.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/libs/jquery/jquery.tokenize.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/libs/jquery/jquery.highlight.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/libs/twitter-bootstrap/bootstrap-tooltip.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/libs/twitter-bootstrap/bootstrap-popover.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/libs/twitter-bootstrap/plugins/bootstrap-classyTooltip.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/libs/twitter-bootstrap/plugins/bootstrap-classyPopover.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/shared/Tracker.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/shared/BrowserDetect.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/shared/LanguageCodes.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/shared/MicrosoftTranslator.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/shared/WordAnalyzer.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/shared/VocabularyManager.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/extension/google-analytics.js', dataType:'text'}),
-			$.ajax({ url:serverBaseUrl+'/Scripts/extension/fleex-page.js', dataType:'text'}),
-		]).done(function() {
-			// Inject dependencies
-			for (var i = 0; i < arguments.length; i++) {
-				// Replace relative urls to absolute urls
-				var code = arguments[i][0].replace(/url(( )*)?:(( )*)?'\//g,"url: '"+serverBaseUrl+"/");
-				// Jsonp calls are not supported in chrome, and we don't need them
-				code = code.replace(/(')?dataType(')?(( )*)?:(( )*)?'jsonp'(( )*)?,/, '');
-				// Inject script into page
-				chrome.tabs.executeScript(tab.id, { 'code': code });
-			};
+			// Fetch (remote) or (static + dynamic) js dependencies
+			$.when.apply($, [
+				$.ajax({ url:serverBaseUrl+'/Scripts/libs/underscore-1.5.1.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/libs/jquery/jquery.tokenize.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/libs/jquery/jquery.highlight.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/libs/twitter-bootstrap/bootstrap-tooltip.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/libs/twitter-bootstrap/bootstrap-popover.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/libs/twitter-bootstrap/plugins/bootstrap-classyTooltip.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/libs/twitter-bootstrap/plugins/bootstrap-classyPopover.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/shared/Tracker.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/shared/BrowserDetect.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/shared/LanguageCodes.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/shared/MicrosoftTranslator.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/shared/WordAnalyzer.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/shared/VocabularyManager.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/extension/google-analytics.js', dataType:'text'}),
+				$.ajax({ url:serverBaseUrl+'/Scripts/extension/fleex-page.js', dataType:'text'}),
+			]).done(function() {
+				// Inject dependencies
+				for (var i = 0; i < arguments.length; i++) {
+					// Replace relative urls to absolute urls
+					var code = arguments[i][0].replace(/url(( )*)?:(( )*)?'\//g,"url: '"+serverBaseUrl+"/");
+					// Jsonp calls are not supported in chrome, and we don't need them
+					code = code.replace(/(')?dataType(')?(( )*)?:(( )*)?'jsonp'(( )*)?,/, '');
+					// Pass variables that are only accessible within the extension to the last script (fleex-page.js)
+					if(i==arguments.length-1){
+						code="var pageData = { 'serverBaseUrl':'"+serverBaseUrl+"', 'iso6391Language':'"+iso6391Language+"' };"+code;
+					}
+					// Inject script into page
+					chrome.tabs.executeScript(tab.id, { 'code': code });
+				};
 
-			// Inject remote css styles
-			$.ajax(serverBaseUrl+'/Content/less/extension/styles.less').done(function(code){
-				// Replace relative urls to absolute urls
-				var code = code.replace(/url\((.*)\)/g,"url("+serverBaseUrl+"$1)").replace(/'/g, '');
-				// Inject styles into page
-	            chrome.tabs.insertCSS(tab.id, { 'code': code })
-			})
+				// Inject remote css styles
+				$.ajax(serverBaseUrl+'/Content/less/extension/styles.less').done(function(code){
+					// Replace relative urls to absolute urls
+					var code = code.replace(/url\((.*)\)/g,"url("+serverBaseUrl+"$1)").replace(/'/g, '');
+					// Inject styles into page
+		            chrome.tabs.insertCSS(tab.id, { 'code': code })
+				})
 
-			// Close popup
-			// window.close();
-		});
+				// Close popup
+				// window.close();
+			});
+		})
+
 	})
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,49 +1,49 @@
 $(function(){
-		function onSuccess (success) {
-			$('body').removeClass('loading');
-			if(success != 'True'){ 
-				onFailure(); 
-			} else{
-				// Get current tab, and fleex it
-				chrome.tabs.query({active:true,currentWindow:true},function(tab){
-					fleexTab(tab[0]);
-				});
-			}
+	function onSuccess (success) {
+		$('body').removeClass('loading');
+		if(success != 'True'){ 
+			onFailure(); 
+		} else{
+			// Get current tab, and fleex it
+			chrome.tabs.query({active:true,currentWindow:true},function(tab){
+				fleexTab(tab[0]);
+			});
 		}
+	}
 
-		function onFailure () {
-			$('header').addClass('error');
-		}
+	function onFailure () {
+		$('header').addClass('error');
+	}
 
-		// Login user
-		$('#submitButton').click(function(){
-			$('header').removeClass('error');
-			$('body').addClass('loading');
-			// Get server base url from settings
-			$.getJSON(chrome.extension.getURL('config.json'), function(settings) {
-				var serverBaseUrl = settings.serverBaseUrl;
-				$.ajax({
-					type:'POST',
-					url:serverBaseUrl+'/Account/BrowserExtensionLogin',
-					data:{
-						'email':$('#Email').val(),
-						'password':$('#Password').val()
-					},
-					error:function(){
-						onFailure();
-					},
-					success:function(success){
-						onSuccess(success);
-					}
-				})
+	// Login user
+	$('#submitButton').click(function(){
+		$('header').removeClass('error');
+		$('body').addClass('loading');
+		// Get server base url from settings
+		$.getJSON(chrome.extension.getURL('config.json'), function(settings) {
+			var serverBaseUrl = settings.serverBaseUrl;
+			$.ajax({
+				type:'POST',
+				url:serverBaseUrl+'/Account/BrowserExtensionLogin',
+				data:{
+					'email':$('#Email').val(),
+					'password':$('#Password').val()
+				},
+				error:function(){
+					onFailure();
+				},
+				success:function(success){
+					onSuccess(success);
+				}
 			})
 		})
+	})
 
-		// Listen to 'Enter' keypresses
-		$('html').keyup(function (event) {
-	        //Enter key code = 13
-	        if (event.keyCode == '13') {
-	            $('#submitButton').click();
-	        }
-	    });
+	// Listen to 'Enter' keypresses
+	$('html').keyup(function (event) {
+        //Enter key code = 13
+        if (event.keyCode == '13') {
+            $('#submitButton').click();
+        }
+    });
 })
