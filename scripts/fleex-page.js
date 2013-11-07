972794448c90cf6fe7a3251c5a47a70b4cfc86dd
Alexandre Point <apoint@fleex.tv>
2013-11-07 16:07:24 +0100

    New: retrieve all infos by fleexing the page

--- scripts/fleex-page.js
+++ scripts/fleex-page.js
@@ -1,9 +1,20 @@
 var serverBaseUrl = "http://localhost:6564";
-$.ajax(serverBaseUrl + '/User/GetUserInfos').done(function(userInfos){
+var url = document.URL;
+var pageLanguageCode = "en";
+var title = document.getElementsByTagName("title")[0].innerHTML;
+$.ajax({
+	url: serverBaseUrl + '/WebPage/Fleex',
+	data: { url: url, languageIsoCode : pageLanguageCode, title: title},
+	type: 'POST'
+}).done(function(infos){
 	debugger;
-	if(userInfos){
-		
-		var vocabularyManager = new VocabularyManager({ code:'eng' }, { code:userInfos.nativeLanguageCode }, 'webPage', 'test');
+	if(infos){
+		var userId = infos.userId;
+		var isInSprint = infos.isInSprint;
+		var webPageId = infos.webPageId;
+		var relationship = infos.relationship;
+		var nativeLanguageCode = infos.nativeLanguageCode;
+		var vocabularyManager = new VocabularyManager({ code:'eng' }, { code: nativeLanguageCode }, 'webPage', 'test');
 		vocabularyManager.init($('body'));
 	}
 });
