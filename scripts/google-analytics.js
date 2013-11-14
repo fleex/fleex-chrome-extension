7ee2149df3c878a52c6089a1455122cdf4849853
Alexandre Point <apoint@fleex.tv>
2013-11-14 18:33:17 +0100

    New: updated extension id

--- scripts/google-analytics.js
+++ scripts/google-analytics.js
@@ -20,6 +20,6 @@ _gaq.push(['_setAccount', 'UA-31019541-1']);
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','chrome-extension://clapakcnmiimhnnocbmabiigfdmnihnb/scripts/analytics.js','ga');
+})(window,document,'script','chrome-extension://gbblinkjgdfpfijckdifhdiijmcpjebj/scripts/analytics.js','ga');
 
 ga('create', 'UA-31019541-2', 'fleex.tv');
